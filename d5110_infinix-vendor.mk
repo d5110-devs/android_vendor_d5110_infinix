bd085b50ca077cae938da1deb09bd624c2ad29f5
HostZero <haneesh.da.rocker@gmail.com>
2016-06-08 18:38:54 +0530

    Marshmallow Bringup

---
+++ d5110_infinix-vendor.mk
@@ -0,0 +1,10 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/infinix/d5110_infinix/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/infinix/d5110_infinix/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/infinix/d5110_infinix/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/infinix/d5110_infinix/vendor,system/vendor)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/infinix/d5110_infinix/xbin,system/xbin)
+
